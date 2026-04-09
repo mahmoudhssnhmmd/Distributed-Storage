08d53efb7d6e6c7d178e7fd5f8332890989eefc0
mahmoudhssnhmmd <157806572+mahmoudhssnhmmd@users.noreply.github.com>
2026-04-09 13:59:20 +0200

    feat: add DTOs for auth

---
+++ src/main/java/springbootproject/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package springbootproject.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String username;
+}
