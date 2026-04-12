bd5d950bc67af6cc5c306f04dd24cae8e7b76207
mahmoudhssnhmmd <157806572+mahmoudhssnhmmd@users.noreply.github.com>
2026-04-12 17:52:57 +0200

    feat: add pagination support for findByUser method in FileMetadataRepository

--- src/main/java/springbootproject/repository/FileMetadataRepository.java
+++ src/main/java/springbootproject/repository/FileMetadataRepository.java
@@ -1,6 +1,8 @@
 package springbootproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import springbootproject.entity.FileMetadata;
 import springbootproject.entity.User;
@@ -11,6 +13,8 @@ import java.util.Optional;
 public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
 
     List<FileMetadata> findByUser(User user);
+
+    Page<FileMetadata> findByUser(User user, Pageable pageable);
 
     Optional<FileMetadata> findByIdAndUser(Long id, User user);
 }
